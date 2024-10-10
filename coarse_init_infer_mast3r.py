6eb0fbf7036a503c4c2431531b05483cf893c081
tackgeun <tackgeun.you@kist.re.kr>
2024-10-10 18:38:01 +0900

    linear interpolation of camera pose

--- coarse_init_infer_mast3r.py
+++ coarse_init_infer_mast3r.py
@@ -10,6 +10,8 @@ os.sys.path.append(os.path.abspath(os.path.join(BASE_DIR, "submodules", "mast3r"
 os.sys.path.append(os.path.abspath(os.path.join(BASE_DIR, "submodules", "mast3r", 'dust3r')))
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
+import torch.nn.functional as F
+
 from dust3r.inference import inference
 #from dust3r.model import AsymmetricCroCo3DStereo
 from mast3r.model import AsymmetricMASt3R
@@ -18,6 +20,16 @@ from dust3r.image_pairs import make_pairs
 from mast3r.cloud_opt.sparse_ga import sparse_global_alignment
 from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
 from utils.dust3r_utils import  compute_global_alignment, load_images, storePly, save_colmap_cameras, save_colmap_images
+
+def bchw_order(t):
+    t = t.unsqueeze(0)
+    t = t.permute(0, 3, 1, 2)
+    return t
+
+def numpy_order(t):
+    t = t.squeeze(0)
+    t = t.permute(1,2,0)
+    return to_numpy(t)
 
 def get_intrinsics(scene): 
     K = torch.zeros((scene.n_imgs, 3, 3)) 
@@ -37,13 +49,21 @@ def get_args_parser():
     parser.add_argument("--schedule", type=str, default='linear')
     parser.add_argument("--lr", type=float, default=0.07)
     parser.add_argument("--niter", type=int, default=500)
+    # parser.add_argument("--lr_reproj", type=float, default=0.014)
+    # parser.add_argument("--niter_reproj", type=int, default=200)    
+    parser.add_argument("--lr_reproj", type=float, default=0.0)
+    parser.add_argument("--niter_reproj", type=int, default=0)        
     parser.add_argument("--focal_avg", action="store_true")
+
 
     parser.add_argument("--llffhold", type=int, default=2)
     parser.add_argument("--n_views", type=int, default=12)
     parser.add_argument("--img_base_path", type=str, default="/home/workspace/datasets/ins/tantsplat/Tanks/Barn/24_views")
 
     parser.add_argument("--scene_graph", type=str, default='swin-3')
+
+    parser.add_argument("--subsample_frame", type=int, default=1)
+    parser.add_argument("--subsample_res", type=float, default=1.0)
 
     return parser
 
@@ -58,6 +78,8 @@ if __name__ == '__main__':
     schedule = args.schedule
     lr = args.lr
     niter = args.niter
+    lr_reproj = args.lr_reproj
+    niter_reproj = args.niter_reproj
     n_views = args.n_views
     img_base_path = args.img_base_path
     scene_graph = args.scene_graph
@@ -69,6 +91,7 @@ if __name__ == '__main__':
 
     train_img_list = sorted(os.listdir(img_folder_path))
     assert len(train_img_list)==n_views, f"Number of images ({len(train_img_list)}) in the folder ({img_folder_path}) is not equal to {n_views}"
+    #images, ori_size = load_images([os.path.join(img_folder_path, img_name) for img_name in train_img_list], size=512)
     images, ori_size = load_images(img_folder_path, size=512)
     print("ori_size", ori_size)
 
@@ -80,23 +103,32 @@ if __name__ == '__main__':
     os.makedirs(output_colmap_path, exist_ok=True)
 
     scene = sparse_global_alignment(train_img_list, pairs, os.path.join('output', 'cache'),
-                                    model, lr1=lr, niter1=niter, lr2=0, niter2=None, device=device,
+                                    model, lr1=lr, niter1=niter, lr2=lr_reproj, niter2=niter_reproj, device=device,
                                     opt_depth=False, shared_intrinsics=True,
-                                    matching_conf_thr=1.0)
+                                    matching_conf_thr=1.5)
         
-    imgs = to_numpy(scene.imgs)
+    # imgs = to_numpy(scene.imgs)
+    # focals = scene.get_focals()
+    # poses = to_numpy(scene.get_im_poses())
+    # pts3d, depthmaps, confidence_masks = scene.get_dense_pts3d()
+    # img_size = confidence_masks[0].size()
+    # pts3d = to_numpy([_pts.cpu().view(img_size[0], img_size[1], -1) for _pts in pts3d])
+    # confidence_masks = to_numpy([_conf.cpu() >= 1.0 for _conf in confidence_masks])
+    # intrinsics = to_numpy(get_intrinsics(scene))
+    imgs = scene.imgs
     focals = scene.get_focals()
     poses = to_numpy(scene.get_im_poses())
     pts3d, depthmaps, confidence_masks = scene.get_dense_pts3d()
     img_size = confidence_masks[0].size()
-    pts3d = to_numpy([_pts.cpu().view(img_size[0], img_size[1], -1) for _pts in pts3d])
-    confidence_masks = to_numpy([_conf.cpu() >= 1.0 for _conf in confidence_masks])
+    pts3d = [_pts.cpu().view(img_size[0], img_size[1], -1) for _pts in pts3d]
+    confidence_masks = [_conf.cpu() >= 1.0 for _conf in confidence_masks]
     intrinsics = to_numpy(get_intrinsics(scene))
+
     ##########################################################################################################################################################################################
     end_time = time.time()
     print(f"Time taken for {n_views} views: {end_time-start_time} seconds")
 
-    # save colmap format
+    # save camera and image data with colmap format
     print('save colmap cameras and images')
     save_colmap_cameras(ori_size, intrinsics, os.path.join(output_colmap_path, 'cameras.txt'))
     save_colmap_images(poses, os.path.join(output_colmap_path, 'images.txt'), train_img_list)
@@ -105,13 +137,29 @@ if __name__ == '__main__':
     np.save(output_colmap_path + "/pts_4_3dgs_all.npy", pts_4_3dgs_all)
     np.save(output_colmap_path + "/focal.npy", np.array(focals.cpu()))
 
+    if args.subsample_frame > 1:
+        ssf = args.subsample_frame
+        #import pdb; pdb.set_trace()
+        print(f'subsampling frames every {ssf} frame.')
+        ssf = args.subsample_frame 
+        pts3d = [value for index, value in enumerate(pts3d) if (index+1) % ssf == 1]
+        confidence_masks = [value for index, value in enumerate(confidence_masks) if (index+1) % ssf == 1]
+        imgs = [value for index, value in enumerate(imgs) if (index+1) % ssf == 1]
+    elif args.subsample_res > 1.0:
+        #import pdb; pdb.set_trace()
+        scale_f = 1.0/args.subsample_res
+        pts3d = [numpy_order(F.interpolate(bchw_order(value), scale_factor=scale_f, mode='bilinear')) for value in pts3d]
+        imgs = [numpy_order(F.interpolate(bchw_order(torch.Tensor(value)), scale_factor=scale_f, mode='bilinear')) for value in imgs]   
+        confidence_masks = [to_numpy(F.interpolate(value.float().unsqueeze(0).unsqueeze(0), scale_factor=scale_f).squeeze(0).squeeze(0).bool()) for value in confidence_masks]
+        
+
+    
     print('concat pts3d')
     pts_4_3dgs = np.concatenate([p[m] for p, m in zip(pts3d, confidence_masks)])
     print('concat images')
     color_4_3dgs = np.concatenate([p[m] for p, m in zip(imgs, confidence_masks)])
     color_4_3dgs = (color_4_3dgs * 255.0).astype(np.uint8)
-    print('store ply')
-
+    
     # avoid process kill due to memory size
     import gc
     del pts3d
@@ -121,4 +169,5 @@ if __name__ == '__main__':
     del pts_4_3dgs_all
     gc.collect()
     
+    print('store ply')    
     storePly(os.path.join(output_colmap_path, "points3D.ply"), pts_4_3dgs, color_4_3dgs)
--- train_joint.py
+++ train_joint.py
@@ -14,7 +14,7 @@ import numpy as np
 import torch
 from random import randint
 from utils.loss_utils import l1_loss, ssim
-from gaussian_renderer import render, network_gui
+from gaussian_renderer import render #, network_gui
 import sys
 from scene import Scene, GaussianModel
 from utils.general_utils import safe_state
@@ -150,18 +150,18 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 scene.save(iteration)
                 save_pose(scene.model_path + 'pose' + f"/pose_{iteration}.npy", gaussians.P, train_cams_init)
 
-            # Densification
+            # # Densification
             # if iteration < opt.densify_until_iter:
-                # Keep track of max radii in image-space for pruning
-                # gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
-                # gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
+            #     # Keep track of max radii in image-space for pruning
+            #     gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
+            #     gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
 
-                # if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
-                #     size_threshold = 20 if iteration > opt.opacity_reset_interval else None
-                #     gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold)
+            #     if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
+            #         size_threshold = 20 if iteration > opt.opacity_reset_interval else None
+            #         gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold)
                 
-                # if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
-                #     gaussians.reset_opacity()
+            #     if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
+            #         gaussians.reset_opacity()
 
             # Optimizer step
             if iteration < opt.iterations:
@@ -254,8 +254,8 @@ if __name__ == "__main__":
     parser.add_argument('--port', type=int, default=6009)
     parser.add_argument('--debug_from', type=int, default=-1)
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
-    parser.add_argument("--test_iterations", nargs="+", type=int, default=[500, 800, 1000, 1500, 2000, 3000, 4000, 5000, 6000, 7_000, 30_000])
-    parser.add_argument("--save_iterations", nargs="+", type=int, default=[5000, 10000, 20000, 40000, 80000])
+    parser.add_argument("--test_iterations", nargs="+", type=int, default=[500, 800, 1000, 2000, 3000, 4000, 5000, 10000, 20000, 40000, 80000, 120000, 160000])
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[5000, 10000, 20000, 40000, 80000, 120000, 160000])
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default = None)
